ec510c392851d5f98ebc1ca57f61f1b007ce10dc
Candice Peters <candicelizabethpeters@gmail.com>
2019-07-31 21:37:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
  lenght = find_element_index.length
- length.times do |index|
-   
+ length.times do |value_to_find|
+   puts |value_to_find|
 end
 
 def find_max_value(array)
